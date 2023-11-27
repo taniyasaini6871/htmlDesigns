823b117d177703d73753827ad07fa20e4858e85c
Taniya Saini <taniyasaini6871@gmail.com>
2023-11-27 13:37:54 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      height : {
+        '80vh': '80%',
+
+        color : {
+          'blueb' : '#6497ce',
+        },
+      
+      },
+      
+        
+    
+  },
+  plugins: [],
+}
+
+
+
